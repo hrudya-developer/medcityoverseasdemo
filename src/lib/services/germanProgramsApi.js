b63ce6c1a73487cc24f170ccb17b4c8a49ab9c2e
Hrudya <hrudya@miak.in>
2026-08-20 11:08:04 +0530

    My latest changes

--- src/lib/services/germanProgramsApi.js
+++ src/lib/services/germanProgramsApi.js
@@ -269,12 +269,8 @@ export const germanProgramsApi =
                         uid = 0,
                         id,
                     }) => ({
-                        url: "/german-programs",
-                        method: "POST",
-                        body: {
-                            uid,
-                            id,
-                        },
+                        url: `/german-programs/${id}?uid=${uid}`,
+                        method: "GET",
                     }),
 
                     transformResponse:
